50368ce968553598c1c909af544bb6407f1af294
robertoaguilar1979 <robertoaguilar1979@gmail.com>
2022-02-08 23:53:08 -0800

    added new file firstUniqueCharacter

---
+++ firstUniqueCharacter/firstUniqueCharacter.js
@@ -0,0 +1,17 @@
+let name = "zzhheellaas";
+
+let findUniqueCharacter  = function(string){
+
+for(const char of string){
+                  
+        if( string.indexOf(char) ===  string.lastIndexOf(char)){
+        return char;
+    }
+
+}
+
+}
+
+
+console.log(findUniqueCharacter(name));
+
