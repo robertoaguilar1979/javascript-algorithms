5940e4217d6c3b17506a64268ab926e197c256e8
robertoaguilar1979 <robertoaguilar1979@gmail.com>
2022-02-20 22:39:05 -0800

    updated files

--- firstUniqueCharacter/firstUniqueCharacter.js
+++ firstUniqueCharacter/firstUniqueCharacter.js
@@ -1,17 +1,14 @@
+//this finds the first unique character using a for of loop
+
 let name = "zzhheellaas";
 
-let findUniqueCharacter  = function(string){
+let findUniqueCharacter = function (string) {
 
-for(const char of string){
-                  
-        if( string.indexOf(char) ===  string.lastIndexOf(char)){
-        return char;
+    for (const char of string) {
+        if (string.indexOf(char) === string.lastIndexOf(char)) {
+            return char;
+        }
     }
-
 }
-
-}
-
 
 console.log(findUniqueCharacter(name));
-
--- multiples/multiples.js
+++ multiples/multiples.js
@@ -1,7 +1,5 @@
 // find multiples of 3 and 5 from 1 to 1000
 
-
-
 let sum = 0;
 let i = 1;
 
