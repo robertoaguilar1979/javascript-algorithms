e165ff9c479bd923a7caba042043f76fbd550259
robertoaguilar1979 <robertoaguilar1979@gmail.com>
2022-03-04 19:21:58 -0800

    added new two number sum version 2

---
+++ twoNumberSum/twoNumberSum2.js
@@ -0,0 +1,27 @@
+let numbers = [];
+for (let i = 0; i <= 100; i++) {
+  numbers.push(i);
+}
+
+let value = 7;
+
+let findTwoSum = function (array, target) {
+  let hash = new Map();
+  const iterator = hash.entries();
+  for (let i = 0; i <= array.length; i++) {
+    let currentNumber = array[i];
+    let difference = target - currentNumber;
+
+    if (hash.has(difference)) {
+      return [hash.get(difference), i];
+    } else {
+      hash.set(currentNumber, true);
+    }
+
+    //console.log(iterator.next().value);
+  }
+
+  return -1;
+};
+
+console.log(findTwoSum(numbers, value));
