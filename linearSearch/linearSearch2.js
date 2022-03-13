8ea838b667d51683a16a99196de550866c10e2f4
robertoaguilar1979 <robertoaguilar1979@gmail.com>
2022-03-13 00:11:39 -0800

    updated linearSearch2 with finding last index of

--- linearSearch/linearSearch2.js
+++ linearSearch/linearSearch2.js
@@ -1,10 +1,11 @@
 //linear search using indexOf
 let numbers = [5, 0, 34, 45, 5];
 
-let find = 45;
+let findFirstIndex = 45;
+let findLastIndex = 5;
 
-let where = numbers.indexOf(find);
-let lastIndex = numbers.lastIndexOf(5);
+let where = numbers.indexOf(findFirstIndex);
+let lastIndex = numbers.lastIndexOf(findLastIndex);
 
-console.log(where);
-console.log(lastIndex);
+console.log(`number:${findFirstIndex} is in index:${where}`);
+console.log(`number:${findLastIndex} is in index:${lastIndex}`);
