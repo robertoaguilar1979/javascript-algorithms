cbb7000647863f42786f07228acd016bd4f3baa2
robertoaguilar1979 <robertoaguilar1979@gmail.com>
2022-03-13 00:00:46 -0800

    updated linear search 2

--- linearSearch/linearSearch2.js
+++ linearSearch/linearSearch2.js
@@ -1,8 +1,10 @@
 //linear search using indexOf
-let numbers = [0, 34, 45];
+let numbers = [5, 0, 34, 45, 5];
 
 let find = 45;
 
 let where = numbers.indexOf(find);
+let lastIndex = numbers.lastIndexOf(5);
 
 console.log(where);
+console.log(lastIndex);
