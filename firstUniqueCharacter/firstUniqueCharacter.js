37375e44d19df6c8d444918b8fa7fbee70166dc3
robertoaguilar1979 <robertoaguilar1979@gmail.com>
2022-04-01 02:27:25 -0700

    fixed incoming git pull conflictfor first unique character

--- firstUniqueCharacter/firstUniqueCharacter.js
+++ firstUniqueCharacter/firstUniqueCharacter.js
@@ -3,19 +3,6 @@
 let name = "zzhheellaas";
 
 let findUniqueCharacter = function (string) {
-<<<<<<< HEAD
-    for (const char of string) {
-        if (string.indexOf(char) === string.lastIndexOf(char)) {
-            return char;
-        }
-
-    }
-
-}
-
-
-console.log(findUniqueCharacter(name));
-=======
   for (const char of string) {
     if (string.indexOf(char) === string.lastIndexOf(char)) {
       return char;
@@ -24,4 +11,3 @@ console.log(findUniqueCharacter(name));
 };
 
 console.log(findUniqueCharacter(name));
->>>>>>> 3ee2d6b65ae01425bdb4e4d62626b58e5f76ae61
