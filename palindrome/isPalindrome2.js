43f3edef6666214236a8fc3b60cea1372287152e
robertoaguilar1979 <robertoaguilar1979@gmail.com>
2022-06-17 20:20:56 -0700

    added new file isPalindrome2 to folder palindrome

---
+++ palindrome/isPalindrome2.js
@@ -0,0 +1,18 @@
+let car = "racecar";
+
+
+let isPalindrome = function (string) {
+    let left = 0;
+    let right = string.length - 1;
+
+    while (left < right) {
+        if (string[left] !== string[right]) {
+            return false;
+        }
+        left++;
+        right--;
+    }
+    return true;
+}
+
+console.log(isPalindrome(car));
