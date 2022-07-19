b9469e028572aeea6026c5dd82875f00a8a76119
robertoaguilar1979 <robertoaguilar1979@gmail.com>
2022-07-18 19:52:20 -0700

    updated readme.md and firstUniqueCharacter4 file.

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # javascript-algorithms
 
 this is a mix of common algorithms.
+
+> tip: These algorithms are not in any particular order.
+> The algorithms rage from easy to medium.
--- firstUniqueCharacter/firstUniqueCharacter4.js
+++ firstUniqueCharacter/firstUniqueCharacter4.js
@@ -4,9 +4,10 @@ let string = "rroobeerrtttocd";
 
 
 let firstUniqueChar = function (string) {
-
+    //hash map object
     let hash = {};
 
+    //loop to add characters to hash and count how many times each character shows up in the string.
     for (const i of string) {
         if (hash[i]) {
             hash[i]++;
