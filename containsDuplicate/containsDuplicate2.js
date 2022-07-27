3ec96528ce2b4433ad077ec1f76317362737f719
robertoaguilar1979 <robertoaguilar1979@gmail.com>
2022-07-26 21:19:37 -0700

    added new file containsDuplicate2 to folder containsDuplicate.

--- containsDuplicate/containsDuplicate2.js
+++ containsDuplicate/containsDuplicate2.js
@@ -1,3 +1,21 @@
 // if there is a duplicate in the array of a number return true.
 
 let numbers = [0, 1, 2, 3, 4, 5, 2];
+
+let findDuplicate = function (array) {
+
+    let hash = new Map;
+
+    for (const number of array) {
+        if (hash.has(number)) {
+            return [hash.get(number), true];
+        } else {
+            hash.set(number, number);
+        }
+    }
+
+    return -1;
+
+}
+
+console.log(findDuplicate(numbers));
